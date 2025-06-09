597173046de0bc72212614c02de1d7e12ed0a539
AVS <vahid.sabetumz@gmail.com>
2025-06-09 17:27:55 +0330

    update main at listener

--- listener-service/main.go
+++ listener-service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"listener/event"
 	"log"
 	"math"
 	"os"
@@ -19,13 +20,19 @@ func main() {
 	}
 
 	defer rabbitConn.Close()
-	log.Println("Connected to rabbitMQ")
+
 	// start listening for messages
-
+	log.Println("Listen and consume rabbitMQ messages")
 	// create consumer
-
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
 	// watch the queue and consume events
-
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func connect() (*amqp.Connection, error) {
@@ -39,6 +46,7 @@ func connect() (*amqp.Connection, error) {
 			fmt.Println("Rabbit not ready yet...")
 			counts++
 		} else {
+			log.Println("Connected to rabbitMQ")
 			connection = c
 			break
 		}
